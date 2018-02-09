146dc0249d75a52348a2b887293809594e2ef977
Giovanni Dalmasso <gioda@users.noreply.github.com>
2018-02-09 17:30:42 +0100

    adding colors.py

---
+++ colors.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#
+# @Author: Dalmasso Giovanni <gioda>
+# @Date:   09-Feb-2018
+# @Email:  giovanni.dalmasso@embl.es
+# @Project: 3D reconstruction
+# @Filename: colors.py
+# @Last modified by:   gioda
+# @Last modified time: 09-Feb-2018
+# @License: MIT
+
+
+"""Collection of basic colors as RGB for plotting (inspired by "Tableau" www.tableau.com)"""
